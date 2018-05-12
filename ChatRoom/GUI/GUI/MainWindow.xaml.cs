d1a31f1ceb256bf7aef19d8bbfe561ae96b65eba
MohsenMohamad <mohsenm@bgu.post.ac.il>
2018-05-12 15:11:35 +0300

    A first look of the first Register/login window, its not a final design for sure , expect a better design and features in the future

--- ChatRoom/GUI/GUI/MainWindow.xaml.cs
+++ ChatRoom/GUI/GUI/MainWindow.xaml.cs
@@ -23,6 +23,56 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+            Login_Screen();
         }
+
+        private void Button_Register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Back_Click(object sender, RoutedEventArgs e)
+        {
+            Clear_Fields();
+            Button_Login.Visibility = Visibility.Visible;
+            Login_Screen();
+        }
+
+        private void Register_Screen_Click(object sender, RoutedEventArgs e)
+        {
+            Register_Screen();
+        }
+
+        private void Register_Screen()
+        {
+            Clear_Fields();
+            Button_Login.Visibility = Visibility.Hidden;
+            Label_ID.Visibility = Visibility.Visible;
+            Text_ID.Visibility = Visibility.Visible;
+            Button_Back.Visibility = Visibility.Visible;
+
+        }
+
+        private void Login_Screen()
+        {
+            Label_ID.Visibility = Visibility.Hidden;
+            Text_ID.Visibility = Visibility.Hidden;
+            Button_Back.Visibility = Visibility.Hidden;
+        }
+
+        private void Clear_Fields()
+        {
+            Text_Name.Text = String.Empty;
+            Box_Password.Clear();
+            Text_ID.Text = String.Empty;
+
+        }
+
+      
     }
 }
